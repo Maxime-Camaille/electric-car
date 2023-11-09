382fe07819da565babbdb152e6bd8a8be019b439
Maxime Camaille <maxime.camaille@gmail.com>
2023-11-09 16:21:03 +0100

    Footer version Desktop OK

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,23 +7,18 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-columns">
-        <div className="footer-column">
-          <img src={LogoFooter} alt="Logo Bornstall" className="footer-logo" />
-          <p className="marketing-phrase">
-            L'avenir électrique pour un monde en mouvement
-          </p>
-        </div>
+        
         <div className="footer-column">
           <h2 className="footer-column-title">Service</h2>
           <ul className="links-list">
             <li>
               <a className="footer-link" href="#">
-                Nos Bornes de recharges
+                Nos bornes de recharges
               </a>
             </li>
             <li>
               <a className="footer-link" href="#">
-                Installation
+                Nos offres pour particulier
               </a>
             </li>
             <li>
@@ -33,12 +28,12 @@ function Footer() {
             </li>
             <li>
               <a className="footer-link" href="#">
-                Devis
+                Obtenir un devis
               </a>
             </li>
             <li>
               <a className="footer-link" href="#">
-                Lorem Ipsum
+                Nos articles
               </a>
             </li>
           </ul>
@@ -66,11 +61,6 @@ function Footer() {
                 Qui sommes nous
               </a>
             </li>
-            <li>
-              <a className="footer-link" href="#">
-                Blog
-              </a>
-            </li>
           </ul>
         </div>
         <div className="footer-column newsletter">
@@ -78,22 +68,26 @@ function Footer() {
             La Newsletter Qui Fait Rouler l'Avenir
           </h2>
           <p className="newsletter-text">
-            Rejoignez notre newsletter pour rester informé sur les dernières
-            avancées de l'installation de bornes électriques. Soyez à
-            l'avant-garde de la mobilité électrique.
+            Soyez à l'avant-garde de la mobilité électrique.
           </p>
           <div className="newsletter-subscribe">
-            <input type="email" placeholder="Entrez votre adresse e-mail" />
-            <button>M'abonner</button>
+            <input type="email" placeholder="E-mail" />
+            <button className="subscribe-newsletter-button">M'abonner</button>
           </div>
           <p className="unsubscribe-text">
             Vous pouvez vous désabonner à tout moment. Nous respectons votre
-            vie privée.{" "}
-            <a href="#">En savoir plus sur notre politique de protection des données.</a>
+            vie privée. Afin d'en savoir plus sur notre politique de protection des données,{" "}
+            <a href="#">cliquez-ici.</a>
           </p>
         </div>
       </div>
       <div className="footer-column copyright">
+      <div className="footer-column-copyright">
+          <img src={LogoFooter} alt="Logo Bornstall" className="footer-logo" />
+          <p className="marketing-phrase">
+            L'avenir électrique pour un monde en mouvement
+          </p>
+        </div>
         <p className="copyright">© Bornstall Tous droits réservés 2023</p>
       </div>
     </footer>
