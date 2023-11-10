6bb959e91050098ba39e5e2ffcdece84b48307f3
Maxime Camaille <maxime.camaille@gmail.com>
2023-11-10 14:38:43 +0100

    Footer OK

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,91 +5,88 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-columns">
-        
-        <div className="footer-column">
-          <h2 className="footer-column-title">Service</h2>
-          <ul className="links-list">
+    <footer className='footer'>
+      <div className='footer-columns'>
+        <div className='footer-column'>
+          <h2 className='footer-column-title'>Service</h2>
+          <ul className='links-list'>
             <li>
-              <a className="footer-link" href="#">
-                Nos bornes de recharges
+              <a className='footer-link' href='#'>
+                <span>Nos bornes de recharges</span>
               </a>
             </li>
             <li>
-              <a className="footer-link" href="#">
-                Nos offres pour particulier
+              <a className='footer-link' href='#'>
+                <span>Nos offres pour particulier</span>
               </a>
             </li>
             <li>
-              <a className="footer-link" href="#">
-                Simulateur
+              <a className='footer-link' href='#'>
+                <span>Simulateur</span>
               </a>
             </li>
             <li>
-              <a className="footer-link" href="#">
-                Obtenir un devis
+              <a className='footer-link' href='#'>
+                <span>Obtenir un devis</span>
               </a>
             </li>
             <li>
-              <a className="footer-link" href="#">
-                Nos articles
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-column">
-          <h2 className="footer-column-title">Informations</h2>
-          <ul className="links-list">
-            <li>
-              <a className="footer-link" href="#">
-                Mentions légales
-              </a>
-            </li>
-            <li>
-              <a className="footer-link" href="#">
-                Conditions générales de ventes
-              </a>
-            </li>
-            <li>
-              <a className="footer-link" href="#">
-                Politique de confidentialité
-              </a>
-            </li>
-            <li>
-              <a className="footer-link" href="#">
-                Qui sommes nous
+              <a className='footer-link' href='#'>
+                <span>Nos articles</span>
               </a>
             </li>
           </ul>
         </div>
-        <div className="footer-column newsletter">
-          <h2 className="footer-column-title">
+        <div className='footer-column'>
+          <h2 className='footer-column-title'>Informations</h2>
+          <ul className='links-list'>
+            <li>
+              <a className='footer-link' href='#'>
+                <span>Mentions légales</span>
+              </a>
+            </li>
+            <li>
+              <a className='footer-link' href='#'>
+                <span>Conditions générales de ventes</span>
+              </a>
+            </li>
+            <li>
+              <a className='footer-link' href='#'>
+                <span>Politique de confidentialité</span>
+              </a>
+            </li>
+            <li>
+              <a className='footer-link' href='#'>
+                <span>Qui sommes nous</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='footer-column newsletter'>
+          <h2 className='footer-column-title'>
             La Newsletter Qui Fait Rouler l'Avenir
           </h2>
-          <p className="newsletter-text">
+          <p className='newsletter-text'>
             Soyez à l'avant-garde de la mobilité électrique.
           </p>
-          <div className="newsletter-subscribe">
-            <input type="email" placeholder="E-mail" />
-            <button className="subscribe-newsletter-button">M'abonner</button>
+          <div className='newsletter-subscribe'>
+            <input type='email' placeholder='E-mail' />
+            <button className='subscribe-newsletter-button'>M'abonner</button>
           </div>
-          <p className="unsubscribe-text">
-            Vous pouvez vous désabonner à tout moment. Nous respectons votre
-            vie privée. Afin d'en savoir plus sur notre politique de protection des données,{" "}
-            <a href="#">cliquez-ici.</a>
+          <p className='unsubscribe-text'>
+            Vous pouvez vous désabonner à tout moment. Nous respectons votre vie
+            privée. Afin d'en savoir plus sur notre politique de protection des
+            données, <a href='#'>cliquez-ici.</a>
           </p>
         </div>
       </div>
-      <div className="footer-column copyright">
-      <div className="footer-column-copyright">
-          <img src={LogoFooter} alt="Logo Bornstall" className="footer-logo" />
-          <p className="marketing-phrase">
+      <div className='footer-column-copyright'>
+          <img src={LogoFooter} alt='Logo Bornstall' className='footer-logo' />
+          <p className='marketing-phrase'>
             L'avenir électrique pour un monde en mouvement
           </p>
-        </div>
-        <p className="copyright">© Bornstall Tous droits réservés 2023</p>
       </div>
+      <p className='copyright'>© Bornstall Tous droits réservés 2023</p>
     </footer>
   );
 }
