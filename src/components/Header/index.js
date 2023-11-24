20ecc3c567afd48ad51660dd6b4d937c8f9927e7
Maxime Camaille <maxime.camaille@gmail.com>
2023-11-24 14:54:50 +0100

    Scroll to top when the route change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
 import "./styles.css";
 import Logo from "../../assets/images/Bornstall.png";
-import React, {useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Header() {
   const [showLinks, setShowLinks] = useState(false);
@@ -18,17 +19,26 @@ function Header() {
       setNavBar(false);
     }
   };
-  
 
   window.addEventListener("scroll", changeBackground);
 
+  // -----------------------AU CHANGEMENT DE ROUTE REMETTRE LE FOCUS SUR LE HEADER ----------------------------//
+  const headerRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Lorsque la route change, déplace le focus vers l'élément du header
+    if (headerRef.current) {
+      headerRef.current.focus();
+    }
+  }, [location.pathname]); // S'assure que l'effet est exécuté lors d'un changement de route
+
+  //----------------------------------------------------------------------------------------------------------------//
+
   return (
-    <header className='header'>
+    <header className='header' tabIndex={-1} ref={headerRef}>
       <nav className={navBar ? "NavBar active" : "NavBar"}>
-        <Link
-          to='/'
-          className='logo'
-        >
+        <Link to='/' className='logo'>
           <img decoding='async' src={Logo} alt='logo-of-the-website' />
         </Link>
         <div
